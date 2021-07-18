2ec0346aae38396ad956720459a45c99c3078a8f
ShubhiAgarwal115 <shubhiagarwal8989@gmail.com>
2021-07-18 18:17:28 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useEffect,useState} from 'react';
+import './App.css'
+const App = ()=>{
+    const [username,setUsername]=useState('');
+    const [password,setPassword] = useState('');
+    const [passwordWant,setPasswordWant]=useState(true);
+    return(
+        <div className="container">
+        <input
+        type="text" placeholder="Enter your username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+        <input
+        type={passwordWant===true ? 'text': 'password'} placeholder="Enter your password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+        <h2>{username}</h2>
+        <h3>{passwordWant===true ? password : '*'.repeat(password.length)}</h3>
+        <button onClick={(e)=>setPasswordWant(!passwordWant)}>Show/Hide your password</button>
+        </div>
+    )
 }
-
 export default App;
